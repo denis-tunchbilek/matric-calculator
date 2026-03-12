5b9cfe815b3a756f537cbbdb0aa691655f7754be
denis-tunchbilek <86554612+denis-tunchbilek@users.noreply.github.com>
2026-03-12 16:16:09 +0300

    matrix_input namespace

--- src/matrix_input.cpp
+++ src/matrix_input.cpp
@@ -1,14 +1,36 @@
 #include "matrix_input.h"
-
+#include <cctype>
 #include <sstream>
 #include <string>
-
 #include "errors.h"
 
+namespace {
+
+    std::string trim(const std::string& s) {
+        std::size_t begin = 0;
+        while (begin < s.size() && std::isspace(static_cast<unsigned char>(s[begin]))) {
+            ++begin;
+        }
+
+        std::size_t end = s.size();
+        while (end > begin && std::isspace(static_cast<unsigned char>(s[end - 1]))) {
+            --end;
+        }
+
+        return s.substr(begin, end - begin);
+    }
+
+    std::string readAll(std::istream& in) {
+        std::ostringstream buffer;
+        buffer << in.rdbuf();
+        return buffer.str();
+    }
+
+}
+
 Matrix MatrixInput::read(std::istream& in) {
-    std::ostringstream buffer;
-    buffer << in.rdbuf();
-    const std::string text = buffer.str();
+    const std::string rawText = readAll(in);
+    const std::string text = trim(rawText);
 
     if (text.empty()) {
         throw ParseError("empty input");
